66f1cf7ec9cb0d35e652f5090c90847ffef8b1a4
erolland2018 <erwan.rolland@wanadoo.fr>
2018-11-27 16:36:32 +0100

    commit

--- src/Admin/UtilisateurAdmin.php
+++ src/Admin/UtilisateurAdmin.php
@@ -12,26 +12,53 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UtilisateurAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('lastname', TextType::class)
+        $formMapper->with('Utilisateur',['class' => 'col-md-9'])
+            ->add('lastname', TextType::class)
             ->add('firstname', TextType::class)
             ->add('email', TextType::class)
             ->add('username',TextType::class)
-            ->add('password',TextType::class);
+            ->add('password',PasswordType::class)
+
+            ->add('roles',ChoiceType::class,
+                array(
+                    'choices' =>array(
+                        'USER' =>'ROLE_USER',
+                        'ADMIN' => 'ROLE_USER'
+                    ),
+                    'multiple'=> true
+                )
+            )
+            ->end();
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('username');
+        $datagridMapper->add('lastname')
+            ->add('firstname')
+            ->add('email')
+            ->add('username')
+            ->add('password')
+            ->add('roles')
+        ;
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('username');
+        $listMapper ->addIdentifier('username')
+                    ->add('lastname')
+                    ->add('firstname')
+                    ->add('email')
+                    ->add('password')
+                    ->add('roles')
+
+        ;
     }
 }
